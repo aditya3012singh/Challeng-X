f27537b9bc8f550022aa28c3819466fc47e8dccf
Aditya <aditya.2428cseai@gmail.com>
2026-02-05 23:31:05 +0530

    Add missing newline at end of SquidMode.jsx file

--- frontend/src/pages/SquidMode.jsx
+++ frontend/src/pages/SquidMode.jsx
@@ -3,6 +3,7 @@ export const SquidMode=()=>{
     return <div className="">
         <div className="min-h-screen bg-[#0f0f12] text-white flex items-center justify-center px-4">
             SquidMode Page
-            </div>
-            </div>
+        </div>
+    </div>
 }
+
