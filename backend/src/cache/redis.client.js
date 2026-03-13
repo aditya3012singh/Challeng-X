bb84e790a037ac8a6c0a1e61716fc3f5deffd8cf
Aditya <aditya.2428cseai@gmail.com>
2026-03-13 19:37:35 +0530

    feat: Implement Redis client and configure daily log rotation.

--- backend/src/cache/redis.client.js
+++ backend/src/cache/redis.client.js
@@ -2,13 +2,17 @@ import Redis from "ioredis";
 
 class RedisClient {
   static client = new Redis({
-    host: "127.0.0.1",
+    host: "redis",
     port: 6379
   });
 }
 
 RedisClient.client.on("connect", () => {
   console.log("✅ Redis connected");
+});
+
+RedisClient.client.on("error", (err) => {
+  console.error("❌ Redis error:", err);
 });
 
 export default RedisClient;
